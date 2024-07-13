aecc2f411854c4fba99145f912a5466591677ec0
Prithviraj Mazumder <prithirajmajumder8@gmail.com>
2024-07-13 19:35:14 +0530

    feat: adds navigation links

---
+++ apps/troithWeb/app/tool/components/navMenu.tsx
@@ -0,0 +1,37 @@
+import { cn } from '@troith/shared/lib/util'
+import { Button, buttonVariants, P, Tooltip, TooltipContent, TooltipTrigger } from '@troith/shared'
+import { LucideIcon, SlidersHorizontal } from 'lucide-react'
+import Link, { LinkProps } from 'next/link'
+import { ReactNode } from 'react'
+
+export type Props = {
+  icon?: LucideIcon
+  children: ReactNode
+  variant?: 'default' | 'ghost' | 'destructive'
+  className?: string
+  iconOnly?: boolean
+} & Omit<LinkProps, 'icon'>
+
+export const NavMenu = ({ children, variant = 'ghost', className, iconOnly = false, ...props }: Props) => {
+  if (iconOnly) {
+    return (
+      <Tooltip delayDuration={0}>
+        <TooltipTrigger asChild>
+          <Link {...props} className={cn(buttonVariants({ variant, size: iconOnly ? 'icon' : 'sm' }), className)}>
+            {props.icon ? <props.icon className="h-5 w-5" /> : null}
+          </Link>
+        </TooltipTrigger>
+        <TooltipContent side="right">
+          <P>{children}</P>
+        </TooltipContent>
+      </Tooltip>
+    )
+  }
+
+  return (
+    <Link {...props} className={cn(buttonVariants({ variant, size: 'sm' }), 'justify-start', className)}>
+      {props.icon ? <props.icon className="mr-2 h-5 w-5" /> : null}
+      {children}
+    </Link>
+  )
+}
--- apps/troithWeb/app/tool/layout.tsx
+++ apps/troithWeb/app/tool/layout.tsx
@@ -13,23 +13,97 @@ import {
   TooltipTrigger
 } from '@troith/shared/components/ui'
 import { H3, P } from '@troith/shared/components/typography'
-import { Download, Pencil, SlidersHorizontal, Trash } from 'lucide-react'
+import {
+  Download,
+  Factory,
+  Landmark,
+  LogOut,
+  Moon,
+  NotebookText,
+  Pencil,
+  Pyramid,
+  ScrollText,
+  Settings,
+  SlidersHorizontal,
+  SquarePercent,
+  Trash,
+  UsersRound
+} from 'lucide-react'
+import { NavMenu } from './components/navMenu'
+import { useState } from 'react'
 
 const ToolLayout = () => {
+  const [isCollapsed, setIsCollapsed] = useState(false)
+
   return (
     <TooltipProvider>
       <ResizablePanelGroup direction="horizontal" className="!h-screen border-r">
-        <ResizablePanel maxSize={15} minSize={5} defaultSize={15} className="h-full hidden md:block">
-          <div>
-            <header className="h-16 flex items-center">
-              <Button variant="ghost" className="text-3xl font-bold">
-                <span className="text-primary">T</span>roith
-              </Button>
-            </header>
+        <ResizablePanel
+          onCollapse={() => setIsCollapsed(true)}
+          onExpand={() => setIsCollapsed(false)}
+          collapsible
+          maxSize={20}
+          minSize={12}
+          defaultSize={20}
+          collapsedSize={4}
+          className="h-full hidden md:block"
+        >
+          <div className="px-4 h-[calc(100%-4.5rem)]">
+            {isCollapsed ? (
+              <header className="h-16 flex items-center justify-center">
+                <Tooltip delayDuration={500}>
+                  <TooltipTrigger asChild>
+                    <h1 className="text-3xl text-primary font-bold text-center cursor-pointer">T</h1>
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    <P>Easy invoice making tool</P>
+                  </TooltipContent>
+                </Tooltip>
+              </header>
+            ) : (
+              <header className="h-16 flex items-center">
+                <h1 className="text-3xl font-bold">
+                  <span className="text-primary">T</span>roith
+                </h1>
+              </header>
+            )}
+            <div style={{ alignItems: isCollapsed ? 'center' : 'start' }} className="py-2 flex-col flex gap-1 h-full">
+              <NavMenu iconOnly={isCollapsed} href="#" icon={ScrollText}>
+                Invoices
+              </NavMenu>
+              <NavMenu iconOnly={isCollapsed} href="#" icon={NotebookText}>
+                Challans
+              </NavMenu>
+              <NavMenu iconOnly={isCollapsed} href="#" icon={Pyramid}>
+                Items
+              </NavMenu>
+              <NavMenu iconOnly={isCollapsed} href="#" icon={UsersRound}>
+                Parties
+              </NavMenu>
+              <Separator className="my-2" />
+              <NavMenu iconOnly={isCollapsed} href="#" icon={Factory}>
+                Companies
+              </NavMenu>
+              <NavMenu iconOnly={isCollapsed} href="#" icon={SquarePercent}>
+                Taxes
+              </NavMenu>
+              <NavMenu iconOnly={isCollapsed} href="#" icon={Landmark}>
+                Banks
+              </NavMenu>
+              <NavMenu iconOnly={isCollapsed} className="mt-auto" href="#" icon={Settings}>
+                Settings
+              </NavMenu>
+              <NavMenu iconOnly={isCollapsed} href="#" icon={Moon}>
+                Dark mode
+              </NavMenu>
+              <NavMenu iconOnly={isCollapsed} href="#" icon={LogOut}>
+                Logout
+              </NavMenu>
+            </div>
           </div>
         </ResizablePanel>
         <ResizableHandle withHandle className="hidden md:flex" />
-        <ResizablePanel defaultSize={85}>
+        <ResizablePanel defaultSize={80}>
           <ResizablePanelGroup direction="horizontal">
             <ResizablePanel defaultSize={50}>
               <header className="border-b px-4 h-16 flex items-center gap-2">
