ea738b4029c89275d4966daeaef7924358b7e8fe
Prithviraj Mazumder <prithirajmajumder8@gmail.com>
2024-12-19 21:59:29 +0530

    feat: integrates list items with new endpoints

--- apps/troithWeb/app/tool/items/(list)/page.tsx
+++ apps/troithWeb/app/tool/items/(list)/page.tsx
@@ -5,21 +5,21 @@ import { buttonVariants } from '@troith/shared'
 import { Plus } from 'lucide-react'
 import { AnimatePresence, motion } from 'framer-motion'
 import { animateBasicMotionOpacity } from '@troithWeb/app/tool/invoices/utils/animations'
-import { useSuspenseQuery } from '@apollo/client'
 import { useCompanyStore } from '@troithWeb/app/tool/stores/CompanySore'
 import { useRouter } from 'next-nprogress-bar'
-import { ItemQueries } from '@troithWeb/app/tool/items/queries/itemQueries'
 import { ItemCard } from '@troithWeb/app/tool/components/itemCard'
 import { ItemType } from '@troithWeb/types/items'
+import { useSuspenseQuery } from '@tanstack/react-query'
+import { itemsKeys } from '@troithWeb/app/tool/queryKeys/items'
+
+const fetchItems = async (companyId: string): Promise<Array<ItemType>> => await (await fetch(`/api/items/company/${companyId}`)).json()
 
 export default function Items() {
   const { selectedCompany } = useCompanyStore()
   const router = useRouter()
-  const { data: itemsData } = useSuspenseQuery(ItemQueries.all, {
-    variables: {
-      companyId: selectedCompany?.id ?? ''
-    },
-    fetchPolicy: 'network-only'
+  const { data: items } = useSuspenseQuery({
+    queryKey: itemsKeys.lists(selectedCompany?.id ?? ''),
+    queryFn: () => fetchItems(selectedCompany?.id ?? '')
   })
 
   return (
@@ -32,7 +32,7 @@ export default function Items() {
         Create item
       </Link>
       <motion.div {...animateBasicMotionOpacity()} className="flex flex-col w-full gap-4 pb-24">
-        {itemsData?.items?.map((item) => (
+        {items?.map((item) => (
           <ItemCard onSelect={(item) => router.push(`/tool/items/${item.id}`)} entity={item as ItemType} key={`item-list-${item?.id}`} />
         ))}
       </motion.div>
